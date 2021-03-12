08b39e29c6c8d3e5d159f167368ddd5591f643d4
deepfriedbananapeppers69 <whydo1hav3nonames@tutanota.com>
2021-03-12 04:02:21 -0800

    added gui for starting and stopping might give up gui way not work with code

--- testing.py
+++ testing.py
@@ -1,54 +1,54 @@
 
 import random 
 from tkinter import *
-def main():
-    lives = 3
-    score = 1
-
-    while lives > 0:
-        SoCal = ["ANA","PSP","BNS","LAX","BUR","ONT","COM","ORG","CPT","SAN","CXL","SMX","FON","VNS","HBC","WHP","JPD","YUM","JSG"]
-        NoCal = ["CCR","PET","EUR","RED","HAY","SAC","MLO","SFO","MRY","SJC","STK"]
-        socal = SoCal
-        nocal = NoCal
-        wrong = []
-        fullList = SoCal + NoCal
-        choice = random.choice(fullList)
-        print(choice)
-        answer = input("socal or nocal:")
-        if "socal" in answer:
-            answer = socal
-        else:
-            answer = nocal
-
-        if choice in answer:
-            print("correct")
-            score = score + 1
-            print("Your score is: " + str(score))
-        else:
-            print("not correct")
-            score = score - 1
-            lives = lives - 1
-            print("You have " + str(lives) + " lives left.")
-            wrong.append(choice)
-        if lives == 0:
-            print("Sorry try again.")
-            print(wrong)
-root = Tk()
-
-topFrame = Frame(root)
-topFrame.pack()
-bottemFrame = Frame(root)
-bottemFrame.pack(side=BOTTOM)
-
-Button1 = Button(topFrame, text="so cal", fg="red")
-Button2 = Button(topFrame, text="no cal", fg="green")
-Button3 = Button(topFrame, text="button3", fg="blue")
-Button4 = Button(bottemFrame, text="button4", fg="orange")
-Button1.pack(side=LEFT)
-Button2.pack(side=RIGHT)
-Button3.pack()
-Button4.pack()
 
 
+class tracgame:
 
+  
+
+    def __init__(self, master):
+        frame = Frame(master, width=400,height=300)
+        frame.pack()
+
+        self.quitButton = Button(frame, text="QUIT", command=frame.quit)
+        self.quitButton.pack(side=LEFT,)
+
+        self.socalbutton = Button(frame, text="START",command=self.main)
+        self.socalbutton.pack(side=RIGHT)
+
+    
+    def main(self):
+        lives = 3
+        score = 1
+
+        while lives > 0:
+            SoCal = ["ANA","PSP","BNS","LAX","BUR","ONT","COM","ORG","CPT","SAN","CXL","SMX","FON","VNS","HBC","WHP","JPD","YUM","JSG"]
+            NoCal = ["CCR","PET","EUR","RED","HAY","SAC","MLO","SFO","MRY","SJC","STK"]
+            socal = SoCal
+            nocal = NoCal
+            fullList = SoCal + NoCal
+            choice = random.choice(fullList)
+            print(choice)
+            answer = input("socal or nocal:")
+            if "socal" in answer:
+                answer = socal
+            else:
+                answer = nocal
+
+            if choice in answer:
+                print("correct")
+                score = score + 1
+                print("Your score is: " + str(score))
+            else:
+                print("not correct")
+                score = score - 1
+                lives = lives - 1
+                print("You have " + str(lives) + " lives left.")
+            if lives == 0:
+                print("sorry try again")
+    
+
+root = Tk() 
+game = tracgame(root)
 root.mainloop()
