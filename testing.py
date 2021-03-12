1ab0e60ed7859bb4e5a9f65be0c91eaaae9ef8c8
deepfriedbananapeppers69 <whydo1hav3nonames@tutanota.com>
2021-03-11 16:30:37 -0800

    added gui

--- testing.py
+++ testing.py
@@ -1,38 +1,5 @@
 
 import random 
-<<<<<<< HEAD
-lives = 3
-score = 1
-while lives > 0:
-    SoCal = ["ANA","PSP","BNS","LAX","BUR","ONT","COM","ORG","CPT","SAN","CXL","SMX","FON","VNS","HBC","WHP","JPD","YUM","JSG"]
-    NoCal = ["CCR","PET","EUR","RED","HAY","SAC","MLO","SFO","MRY","SJC","STK"]
-    socal = SoCal
-    nocal = NoCal
-    wrong = []
-    strwrong = wrong
-    fullList = SoCal + NoCal
-    choice = random.choice(fullList)
-    print(choice)
-    answer = input("socal or nocal: ")
-    if "socal" in answer:
-        answer = socal
-    else:
-        answer = nocal
-
-    if choice in answer:
-        print("correct")
-        score = score + 1
-        print("Your score is: " + str(score))
-    else:
-        print("not correct")
-        score = score - 1
-        lives = lives - 1
-        wrong.append(choice)
-        print("You have " + str(lives) + " lives left.")
-    if lives == 0:
-        print("Sorry try again.")
-        print(str(strwrong))
-=======
 from tkinter import *
 def main():
     lives = 3
@@ -85,4 +52,3 @@ Button4.pack()
 
 
 root.mainloop()
->>>>>>> bde5f5a... starting on the gui pushed the wrong list off to the side for now want to make it look pretty
