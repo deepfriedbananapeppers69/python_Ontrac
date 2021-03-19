f4e726f592ebe4a82a4e5b174d5b1289a41ef8b4
deepfriedbananapeppers69 <whydo1hav3nonames@tutanota.com>
2021-03-19 01:28:28 -0700

    finshed version 1

--- testing.py
+++ testing.py
@@ -8,47 +8,83 @@ class tracgame:
   
 
     def __init__(self, master):
-        frame = Frame(master, width=400,height=300)
+        # Global Variables
+        # When calling these variables use .self first 
+        self.lives = 3
+        self.score = 0
+        self.SoCal = ["ANA","PSP","BNS","LAX","BUR","ONT","COM","ORG","CPT","SAN","CXL","SMX","FON","VNS","HBC","WHP","JPD","YUM","JSG"]
+        self.NoCal = ["CCR","PET","EUR","RED","HAY","SAC","MLO","SFO","MRY","SJC","STK"]
+        self.socal = self.SoCal
+        self.nocal = self.NoCal
+        self.wrong = []
+        self.fullList = self.SoCal + self.NoCal
+        self.choice = random.choice(self.fullList)
+        
+        
+        # This is the GUI 
+        # ---------------------------------------
+        # This parts makes the frame and for updateing labels 
+        frame = Frame(master)
         frame.pack()
 
-        self.quitButton = Button(frame, text="QUIT", command=frame.quit)
-        self.quitButton.pack(side=LEFT,)
+        self.text = StringVar()
 
-        self.socalbutton = Button(frame, text="START",command=self.main)
-        self.socalbutton.pack(side=RIGHT)
+        self.pickchoice = StringVar()
+        self.pickchoice.set(self.choice)
 
-    
-    def main(self):
-        lives = 3
-        score = 1
+        self.numberscore = StringVar()
+        self.numberscore.set(self.score)
+        # Main parts of the Gui that you see
+        self.Score = Label(frame, textvariable=self.numberscore)
+        self.Score.pack(side=TOP, pady=1, padx=1)
 
-        while lives > 0:
-            SoCal = ["ANA","PSP","BNS","LAX","BUR","ONT","COM","ORG","CPT","SAN","CXL","SMX","FON","VNS","HBC","WHP","JPD","YUM","JSG"]
-            NoCal = ["CCR","PET","EUR","RED","HAY","SAC","MLO","SFO","MRY","SJC","STK"]
-            socal = SoCal
-            nocal = NoCal
-            fullList = SoCal + NoCal
-            choice = random.choice(fullList)
-            print(choice)
-            answer = input("socal or nocal:")
-            if "socal" in answer:
-                answer = socal
-            else:
-                answer = nocal
+        self.label2 = Label(frame, textvariable=self.text, width= 10, height= 2 )
+        self.label2.pack(side=BOTTOM)
 
-            if choice in answer:
-                print("correct")
-                score = score + 1
-                print("Your score is: " + str(score))
-            else:
-                print("not correct")
-                score = score - 1
-                lives = lives - 1
-                print("You have " + str(lives) + " lives left.")
-            if lives == 0:
-                print("sorry try again")
-    
+        self.label1 = Label(frame, textvariable=self.pickchoice)
+        self.label1.pack(side=TOP)
 
+        self.quitButton = Button(frame, text="  QUIT  ",width= 10, height= 2 ,command=frame.quit)
+        self.quitButton.pack(side=BOTTOM)
+
+        self.socalbutton = Button(frame, text="So Cal",width= 10, height= 2 ,command=self.SOCALCHECK)
+        self.socalbutton.pack(side=RIGHT, pady=1, padx=1)
+
+        self.nocalbutton = Button(frame, text="No Cal",width= 10, height= 2 ,command=self.NOCALCHECK)
+        self.nocalbutton.pack(side=RIGHT, pady=2, padx=2)
+        # end of GUI
+        # -----------------------------------------
+
+    # These are fuctions for checking the answers    
+    # These functions will choose a new area code only when you pick the right choice for now  
+    def NOCALCHECK (self):
+        answer = self.choice
+        if answer in self.NoCal:
+            self.score =  self.score + 1
+            self.numberscore.set(self.score)
+            self.text.set("correct")
+            self.choice = random.choice(self.fullList)
+            self.pickchoice.set(self.choice)
+        else: 
+            self.score =  self.score - 1
+            self.numberscore.set(self.score)
+            self.text.set("not correct")
+
+    def SOCALCHECK (self):
+        answer = self.choice
+        if answer in self.SoCal:
+            self.score =  self.score + 1
+            self.numberscore.set(self.score)
+            self.text.set("correct")
+            self.choice = random.choice(self.fullList)
+            self.pickchoice.set(self.choice)
+        else: 
+            self.score =  self.score - 1
+            self.numberscore.set(self.score)
+            self.text.set("not correct")
+           
+   
 root = Tk() 
 game = tracgame(root)
+root.geometry("800x400")
 root.mainloop()
