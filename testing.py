bdd7ee056cd383a2a4f3912771833e45401cd7b5
deepfriedbananapeppers69 <whydo1hav3nonames@tutanota.com>
2021-03-19 03:00:43 -0700

    version 2 of the game

--- testing.py
+++ testing.py
@@ -3,9 +3,8 @@ import random
 from tkinter import *
 
 
-class tracgame:
 
-  
+class tracgame:
 
     def __init__(self, master):
         # Global Variables
@@ -14,12 +13,11 @@ class tracgame:
         self.score = 0
         self.SoCal = ["ANA","PSP","BNS","LAX","BUR","ONT","COM","ORG","CPT","SAN","CXL","SMX","FON","VNS","HBC","WHP","JPD","YUM","JSG"]
         self.NoCal = ["CCR","PET","EUR","RED","HAY","SAC","MLO","SFO","MRY","SJC","STK"]
-        self.socal = self.SoCal
-        self.nocal = self.NoCal
-        self.wrong = []
-        self.fullList = self.SoCal + self.NoCal
+        self.Desert = ["BKY","GEG","BOI","PHX","CDC","RNO","COS","TRI","DIA","TUC","FLG","UTA","BFL","VEG","VIS","FAT"]
+        self.Vancouver = ["VAN"]
+        self.Seattle = ["SEA","SEZ"]
+        self.fullList = self.SoCal + self.NoCal + self.Desert
         self.choice = random.choice(self.fullList)
-        
         
         # This is the GUI 
         # ---------------------------------------
@@ -29,16 +27,22 @@ class tracgame:
 
         self.text = StringVar()
 
+        self.Lives = StringVar()
+        self.Lives.set(" You have Lives: " + (str(self.lives)) + " left") 
+
         self.pickchoice = StringVar()
-        self.pickchoice.set(self.choice)
+        self.pickchoice.set(" Area Code: " + str(self.choice))
 
         self.numberscore = StringVar()
-        self.numberscore.set(self.score)
+        self.numberscore.set("Your Score: " + str(self.score))
         # Main parts of the Gui that you see
         self.Score = Label(frame, textvariable=self.numberscore)
         self.Score.pack(side=TOP, pady=1, padx=1)
 
-        self.label2 = Label(frame, textvariable=self.text, width= 10, height= 2 )
+        self.label3 = Label(frame, textvariable=self.Lives)
+        self.label3.pack(side=TOP, pady=1, padx=1)
+
+        self.label2 = Label(frame, textvariable=self.text)
         self.label2.pack(side=BOTTOM)
 
         self.label1 = Label(frame, textvariable=self.pickchoice)
@@ -51,7 +55,17 @@ class tracgame:
         self.socalbutton.pack(side=RIGHT, pady=1, padx=1)
 
         self.nocalbutton = Button(frame, text="No Cal",width= 10, height= 2 ,command=self.NOCALCHECK)
+        self.nocalbutton.pack(side=RIGHT, pady=2, padx=2)\
+
+        self.nocalbutton = Button(frame, text="Desert",width= 10, height= 2 ,command=self.DESERTCHECK)
         self.nocalbutton.pack(side=RIGHT, pady=2, padx=2)
+
+        self.nocalbutton = Button(frame, text="Seattle",width= 10, height= 2 ,command=self.SEATTLECHECK)
+        self.nocalbutton.pack(side=RIGHT, pady=2, padx=2)
+
+        self.nocalbutton = Button(frame, text="Vancouver",width= 10, height= 2 ,command=self.VANCOUVERCHECK)
+        self.nocalbutton.pack(side=RIGHT, pady=2, padx=2)
+        
         # end of GUI
         # -----------------------------------------
 
@@ -61,28 +75,84 @@ class tracgame:
         answer = self.choice
         if answer in self.NoCal:
             self.score =  self.score + 1
-            self.numberscore.set(self.score)
-            self.text.set("correct")
+            self.numberscore.set("Your Score: " + str(self.score))
+            self.text.set("Your Answer is: correct")
             self.choice = random.choice(self.fullList)
-            self.pickchoice.set(self.choice)
+            self.pickchoice.set(" Area Code: " + str(self.choice))
         else: 
             self.score =  self.score - 1
-            self.numberscore.set(self.score)
-            self.text.set("not correct")
+            self.lives = self.lives - 1
+            self.numberscore.set("Your Score: " + str(self.score))
+            self.text.set("Your Answer is: not correct")
+            self.Lives.set(" You have Lives: " + (str(self.lives)) + " left") 
+        if self.lives <= 0:
+            root.destroy()
 
     def SOCALCHECK (self):
         answer = self.choice
         if answer in self.SoCal:
             self.score =  self.score + 1
-            self.numberscore.set(self.score)
-            self.text.set("correct")
+            self.numberscore.set("Your Score: " + str(self.score))
+            self.text.set("Your Answer is: correct")
             self.choice = random.choice(self.fullList)
-            self.pickchoice.set(self.choice)
+            self.pickchoice.set(" Area Code: " + str(self.choice))
         else: 
             self.score =  self.score - 1
-            self.numberscore.set(self.score)
-            self.text.set("not correct")
-           
+            self.lives = self.lives - 1
+            self.numberscore.set("Your Score: " + str(self.score))
+            self.text.set("Your Answer is: not correct")
+            self.Lives.set(" You have Lives: " + (str(self.lives)) + " left") 
+        if self.lives <= 0:
+            root.destroy()
+    
+    def DESERTCHECK (self):
+        answer = self.choice
+        if answer in self.Desert:
+            self.score =  self.score + 1
+            self.numberscore.set("Your Score: " + str(self.score))
+            self.text.set("Your Answer is: correct")
+            self.choice = random.choice(self.fullList)
+            self.pickchoice.set(" Area Code: " + str(self.choice))
+        else: 
+            self.score =  self.score - 1
+            self.lives = self.lives - 1
+            self.numberscore.set("Your Score: " + str(self.score))
+            self.text.set("Your Answer is: not correct")
+            self.Lives.set(" You have Lives: " + (str(self.lives)) + " left") 
+        if self.lives <= 0:
+            root.destroy()
+    def VANCOUVERCHECK (self):  
+        answer = self.choice
+        if answer in self.Vancouver:
+            self.score =  self.score + 1
+            self.numberscore.set("Your Score: " + str(self.score))
+            self.text.set("Your Answer is: correct")
+            self.choice = random.choice(self.fullList)
+            self.pickchoice.set(" Area Code: " + str(self.choice))
+        else: 
+            self.score =  self.score - 1
+            self.lives = self.lives - 1
+            self.numberscore.set("Your Score: " + str(self.score))
+            self.text.set("Your Answer is: not correct")
+            self.Lives.set(" You have Lives: " + (str(self.lives)) + " left") 
+        if self.lives <= 0:
+            root.destroy()
+    def SEATTLECHECK (self):
+        answer = self.choice
+        if answer in self.Seattle:
+            self.score =  self.score + 1
+            self.numberscore.set("Your Score: " + str(self.score))
+            self.text.set("Your Answer is: correct")
+            self.choice = random.choice(self.fullList)
+            self.pickchoice.set(" Area Code: " + str(self.choice))
+        else: 
+            self.score =  self.score - 1
+            self.lives = self.lives - 1
+            self.numberscore.set("Your Score: " + str(self.score))
+            self.text.set("Your Answer is: not correct")
+            self.Lives.set(" You have Lives: " + (str(self.lives)) + " left") 
+        if self.lives <= 0:
+            root.destroy()
    
 root = Tk() 
 game = tracgame(root)
